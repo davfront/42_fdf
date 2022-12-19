63aff1beacfb43461f901f0d97718155726449e9
davfront <contact@david-pereira.net>
2022-12-19 10:40:34 +0100

    libft

---
+++ libft/include/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 11:42:08 by dapereir          #+#    #+#             */
+/*   Updated: 2022/11/14 11:27:57 by dapereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "libft_char.h"
+# include "libft_mem.h"
+# include "libft_str.h"
+# include "libft_write.h"
+# include "libft_lst.h"
+# include "libft_gnl.h"
+
+#endif
