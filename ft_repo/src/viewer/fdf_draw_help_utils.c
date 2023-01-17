77da1185af2d6f045b9ba7d15cd623928a1a6e17
David Pereira <dapereir@z2r12p2.42lyon.fr>
2023-01-17 11:12:20 +0100

    fix norminette

--- ft_repo/src/viewer/fdf_draw_help_utils.c
+++ ft_repo/src/viewer/fdf_draw_help_utils.c
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 12:52:54 by dapereir          #+#    #+#             */
-/*   Updated: 2023/01/17 10:28:14 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/01/17 11:10:39 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	fdf_draw_label(t_fdf *fdf, int line, char *label)
 {
-	mlx_string_put(fdf->mlx, fdf->win, 24, HELP_OFFSET_Y + 20 * line, WHITE, label);
+	mlx_string_put(fdf->mlx, fdf->win, \
+		24, HELP_OFFSET_Y + 20 * line, WHITE, label);
 }
 
 void	fdf_draw_value(t_fdf *fdf, int line, char *value, int color)
 {
-	mlx_string_put(fdf->mlx, fdf->win, HELP_VALUE_X, HELP_OFFSET_Y + 20 * line, color, value);
+	mlx_string_put(fdf->mlx, fdf->win, \
+		HELP_VALUE_X, HELP_OFFSET_Y + 20 * line, color, value);
 }
 
 void	fdf_draw_info(t_fdf *fdf, int line, char *label, char *value)
