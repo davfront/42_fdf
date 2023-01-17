decf839eae588d62e63fab0c50572659a5dbbbcb
davfront <contact@david-pereira.net>
2023-01-17 16:22:36 +0100

    fix initial center

--- ft_repo/src/viewer/fdf_update_proj.c
+++ ft_repo/src/viewer/fdf_update_proj.c
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:35:43 by dapereir          #+#    #+#             */
-/*   Updated: 2023/01/14 11:01:01 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/01/17 16:21:58 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void	fdf_update_proj(t_fdf *fdf)
 	v = fdf->viewer;
 	ui = fdf->ui;
 	fdf_matrix_init(p);
-	fdf_matrix_translate(p, -m.size_x / 2, -m.size_y / 2);
+	fdf_matrix_translate(p, (1 - (float)m.size_x) / 2, (1 - (float)m.size_y) / 2);
 	fdf_matrix_scale(p, v.zoom, v.zoom, v.zoom * v.z_scale);
 	fdf_matrix_multiply(p, v.rot);
 	fdf_matrix_rotate_y(p, -ui.mouse_dx * ui.mouse_right_btn * PI / 500);
 	fdf_matrix_rotate_x(p, ui.mouse_dy * ui.mouse_right_btn * PI / 500);
-	fdf_matrix_translate(p, v.cx + ui.mouse_dx * ui.mouse_left_btn, \
+	fdf_matrix_translate(p, v.cx + ui.mouse_dx * ui.mouse_left_btn, 
 		v.cy + ui.mouse_dy * ui.mouse_left_btn);
 	ft_memcpy(fdf->viewer.proj, p, sizeof(p));
 }
