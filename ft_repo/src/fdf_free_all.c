4ff145ff215a17b5b6fc6ec8b2284d813f8294ad
davfront <contact@david-pereira.net>
2023-01-18 11:00:50 +0100

    fix leaks

--- ft_repo/src/fdf_free_all.c
+++ ft_repo/src/fdf_free_all.c
@@ -6,7 +6,7 @@
 /*   By: dapereir <dapereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:35:43 by dapereir          #+#    #+#             */
-/*   Updated: 2023/01/18 10:37:46 by dapereir         ###   ########.fr       */
+/*   Updated: 2023/01/18 10:56:39 by dapereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@ void	fdf_free_all(t_fdf *fdf)
 {
 	fdf_free_map(fdf);
 	fdf_free_map_proj(fdf);
-	if (fdf->img.img)
-		mlx_destroy_image(fdf->mlx, fdf->img.img);	
-	if (fdf->win)
-		mlx_destroy_window(fdf->mlx, fdf->win);
-	ft_free(&fdf->mlx);
+	if (fdf->mlx)
+	{
+		if (fdf->img.img)
+			mlx_destroy_image(fdf->mlx, fdf->img.img);
+		if (fdf->win)
+			mlx_destroy_window(fdf->mlx, fdf->win);
+		mlx_destroy_display(fdf->mlx);
+		free(fdf->mlx);
+	}
 }
